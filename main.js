c38f8bf8f0981cef56853dae445e02fde71a3b3a
KikiCodes1 <95891807+KikiCodes1@users.noreply.github.com>
2023-01-25 19:57:17 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+difference = 0;
+rightWrist = 0;
+leftWrist = 0;
+
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+    video.position(400, 170);
+
+    canvas = createCanvas(550, 500);
+    canvas.position(1000, 170);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw(){
+    background('#ADD8E6');
+    textSize(difference);
+    fill("red");
+    text("KIARA ROCKS!!", 50, 400); 
+}
+
+function modelLoaded(){
+    console.log("PoseNet has been initialized!");
+}
+
+function gotPoses(results){
+    if(results > 0){
+        console.log(results);
+        rightWrist = results[0].pose.rightWrist.x;
+        leftWrist = results[0].pose.leftWrist.x;
+        difference = floor(leftWrist - rightWrist);
+    }
+}
